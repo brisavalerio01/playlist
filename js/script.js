cbbab6b8c7cdc467ed66f3dbe9985753e9d5d1bc
Brisa Valerio <brisavalerio116@gmail.com>
2019-01-15 23:20:42 +0000

    Starter

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ['Smoke and Guns', 'One Reason', 'Onion', 'Animal'];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
+var image_url=['https://i.ytimg.com/vi/ehc8w0qsEvY/maxresdefault.jpg', 'http://3.bp.blogspot.com/-l1-rhj64UhM/UWmnbr3U_5I/AAAAAAAAAVk/b5gXc4p6-ws/s1600/One+reason.jpg',
+'http://www.billboard-japan.com/scale/news/00000010/10939/500x_image.jpg', 'https://thealternativesource.files.wordpress.com/2011/10/cabcabcab.jpg'
+];
 
+var artist=['Nathan Sharp', 'Fade', 'One Ok Rock', 'The Cab'];
+
+var song_length=['3:27', '4:23', '3:19', '3:52'];
+
+var link=[];
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
